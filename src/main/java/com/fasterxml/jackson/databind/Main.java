d8981e0857a4890b3e7d07b4d8587f2bbe6ce345
catou88 <100166405+catou88@users.noreply.github.com>
2023-09-25 21:34:47 -0400

    Add files via upload

---
+++ src/main/java/com/fasterxml/jackson/databind/Main.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        URL url = new URL("https://api.api-ninjas.com/v1/exercises?muscle=biceps");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept", "application/json");
+        connection.addRequestProperty("X-Api-Key", "KE8a7QjrGwSZ2jx3+4URNg==aPNxCl8ULpu4Trvb");
+        InputStream responseStream = connection.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseStream);
+        System.out.println(root);
+    }
+}
